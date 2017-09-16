9977d756910a7bdecf36440d774709592198a2cd
Intelicopter <intelicopter@gmail.com>
2017-09-16 14:20:01 +0800

    edit

--- halal/urls.py
+++ halal/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^process-image/', views.process_image, name='process_image'),
 ]
---
+++ halal/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+import urllib
+import os
+from colorthief import ColorThief
+
+
+def home(request):
+    return render(request, 'index.html', {})
+
+
+def process_image(request):
+    url = request.POST['imgBase64']
+    print(dominant_color_from_url(url))
+    return render(request, 'index.html', {})
+
+
+def dominant_color_from_url(url,tmp_file='tmp.jpg'):
+    '''Downloads ths image file and analyzes the dominant color'''
+    urllib.urlretrieve(url, tmp_file)
+    color_thief = ColorThief(tmp_file)
+    dominant_color = color_thief.get_color(quality=1)
+    os.remove(tmp_file)
+    return dominant_color
